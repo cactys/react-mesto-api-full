72f9f4e8ffc222c342fc23069fc9580d1ec18071
cactys95 <cactys95@yandex.ru>
2022-10-05 09:23:15 +0300

    feat: accept Accept: "application/json",

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -30,7 +30,7 @@ class Auth {
       method: 'POST',
       credentials: 'include',
       headers: {
-        Accept: "application/json",
+        'Accept': "application/json",
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
