b3016f5b674f3ebedb51532e3e81b251028061db
cactys95 <cactys95@yandex.ru>
2022-10-05 23:51:25 +0300

    fix: 100500

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -90,7 +90,7 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          // sameSite: 'none',
+          sameSite: true,
           // secure: true,
         })
         .send({
