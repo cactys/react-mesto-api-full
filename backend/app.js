a2ab04c150d0ee77877ef8aea4b6cc523c198d99
cactys95 <cactys95@yandex.ru>
2022-10-09 12:44:56 +0300

    fix: ya zadimelsya

--- backend/app.js
+++ backend/app.js
@@ -14,19 +14,19 @@ const NotFoundError = require('./errors/not-found-err');
 const { ERROR_500 } = require('./utils/code');
 // const { allowedCors } = require('./utils/allowed-cors');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3030 } = process.env;
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cookieParser());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
-
-app.listen(PORT);
 
 app.use(requestLogger); // логгер запросов
 
